5583fd373d6a97991d62b2f7ac5dba4dbad0ca8d
Zharzhan Kulmyrza <zharzhan@gmail.com>
2017-11-24 18:49:38 +0600

    Trying `/docs` for deployment

---
+++ docs/_nuxt/layouts/default.a245bb41eb7075b25c50.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{EzRL:function(e,n,t){"use strict";var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",[t("nuxt")],1)},u=[],l={render:r,staticRenderFns:u};n.a=l},Ma2J:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=t("EzRL"),u=t("VU/8"),l=u(null,r.a,!1,null,null,null);n.default=l.exports}});
+//# sourceMappingURL=default.a245bb41eb7075b25c50.js.map
