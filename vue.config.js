35de09f595f35d1172c7e7784be62393bb972239
Zharzhan Kulmyrza <zharzhan@gmail.com>
2019-01-05 18:15:46 +0600

    Format files (prettier)

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   pwa: {
-    name: "jarjan.xyz",
-    themeColor: "#4a90e2",
-    msTileColor: "#4a90e2"
+    name: 'jarjan.xyz',
+    themeColor: '#4a90e2',
+    msTileColor: '#4a90e2',
   },
   pluginOptions: {
     meta: {
-      projectName: "jarjan.xyz",
-      url: "https://jarjan.xyz",
-      description: "Personal website of Zharzhan Kulmyrza",
-      twitterHandle: "jarjanxyz"
-    }
+      projectName: 'jarjan.xyz',
+      url: 'https://jarjan.xyz',
+      description: 'Personal website of Zharzhan Kulmyrza',
+      twitterHandle: 'jarjanxyz',
+    },
   },
-  lintOnSave: true
+  lintOnSave: true,
 };
