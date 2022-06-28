d25871b4b18e28b40e7f857e1c9b0e055dfaa83a
Zharzhan Kulmyrza <hey@jarjan.xyz>
2022-06-29 00:13:19 +0200

    Add rss support

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/static": "./" });
+
+  eleventyConfig.addPlugin(pluginRss);
 
   return {
     dir: {
