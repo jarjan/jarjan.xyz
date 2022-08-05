d01c5a4c29c1e84bd8e8a9f7e2eeb51d4cf4dc27
Zharzhan Kulmyrza <hey@jarjan.xyz>
2022-08-05 23:06:30 +0200

    Upgrade dependencies

--- src/_data/googleFontsStylesheet.js
+++ src/_data/googleFontsStylesheet.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const url =
   "https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@400;700;800&display=swap";
