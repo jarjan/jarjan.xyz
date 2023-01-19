b6fc88f6dad4e47e8506e9dca1b314211dc07f68
Zharzhan Kulmyrza <hey@jarjan.xyz>
2023-01-19 10:41:14 +0100

    update to 11ty@v2

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
-const pluginRss = require("@11ty/eleventy-plugin-rss");
+const PluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/static": "./" });
 
-  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(PluginRss);
 
   return {
     dir: {
