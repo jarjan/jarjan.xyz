5911fb4b0f4ef370867538b3726e206964bd5c1d
Zharzhan Kulmyrza <hey@jarjan.xyz>
2023-08-08 22:43:15 +0200

    Slowly sunsetting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,3 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: ["./src/**/*.{html,js,njk,md}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Alegreya Sans", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
 };
