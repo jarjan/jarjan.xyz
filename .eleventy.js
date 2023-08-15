ad5a6e0874e4e49b59507b384f3349a22a17fdf2
Zharzhan Kulmyrza <hey@jarjan.xyz>
2023-08-15 21:45:27 +0200

    PostCSS → Lightning CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const PluginRss = require("@11ty/eleventy-plugin-rss");
+const PluginLightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/static": "./" });
 
   eleventyConfig.addPlugin(PluginRss);
+  eleventyConfig.addPlugin(PluginLightningCSS);
 
   return {
     dir: {
