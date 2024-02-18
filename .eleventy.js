a1868bf4d0b76c8c7decb21a25880f7c7c4100ff
Zharzhan Kulmyrza <hey@jarjan.xyz>
2024-02-18 01:34:07 +0100

    fixing redirects, trying to create 404 page and added sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
-const PluginRss = require("@11ty/eleventy-plugin-rss");
-const PluginLightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginLightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
+const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
 
-module.exports = function (eleventyConfig) {
+module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({ "src/static": "./" });
 
-  eleventyConfig.addPlugin(PluginRss);
-  eleventyConfig.addPlugin(PluginLightningCSS);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginLightningCSS);
+  eleventyConfig.addPlugin(pluginSitemap, {
+    sitemap: {
+      hostname: "https://jarjan.xyz",
+    },
+  });
 
   return {
     dir: {
---
+++ src/blog/staff-software-engineer-kim.md
@@ -0,0 +1,8 @@
+---
+layout: layouts/blogpost.njk
+tags: blog
+date: 2023-11-11
+title: Staff Software Engineer деген кім?
+description:
+lang: kk
+---
