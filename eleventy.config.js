f22b038d668fbdf8ddb6c0f10e400a5b804f0c29
Zharzhan Kulmyrza <hey@jarjan.xyz>
2024-08-01 22:30:18 +0200

    almost finished with berlin baby bureaucracy post

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,12 @@
 import pluginRss from "@11ty/eleventy-plugin-rss";
 import pluginLightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
 import pluginSitemap from "@quasibit/eleventy-plugin-sitemap";
+import pluginTimeToRead from "eleventy-plugin-time-to-read";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/static": "./" });
 
+  eleventyConfig.addPlugin(pluginTimeToRead);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginLightningCSS);
   eleventyConfig.addPlugin(pluginSitemap, {
--- src/_data/googleFontsStylesheet.js
+++ src/_data/googleFontsStylesheet.js
@@ -2,6 +2,6 @@ const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const url =
-  "https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@400;700;800&display=optional";
+  "https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@400;700;800&display=swap";
 
 export default fetch(url).then((res) => res.text());
