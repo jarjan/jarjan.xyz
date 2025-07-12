44f88e5432a985371dd510a87b66f3b59e480b0d
Zharzhan Kulmyrza <hey@jarjan.xyz>
2025-07-12 22:28:33 +0200

    minify html

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,9 +2,28 @@ import pluginRss from "@11ty/eleventy-plugin-rss";
 import pluginLightningCSS from "@11tyrocks/eleventy-plugin-lightningcss";
 import pluginSitemap from "@quasibit/eleventy-plugin-sitemap";
 import pluginTimeToRead from "eleventy-plugin-time-to-read";
+import htmlmin from "html-minifier-terser";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/static": "./" });
+
+  // minified HTML
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+
+      return minified;
+    }
+
+    // If not an HTML output, return content as-is
+    return content;
+  });
 
   eleventyConfig.addPlugin(pluginTimeToRead);
   eleventyConfig.addPlugin(pluginRss);
